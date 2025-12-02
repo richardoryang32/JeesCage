a2135919983b0ddf89686a44a8ff7a207b4d8821
niiric32-code <niiric32-code@users.noreply.github.com>
2025-12-02 09:21:18 -0800

    chore: simplify prisma.config.ts format

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,5 @@
-import { defineConfig } from '@prisma/internals';
-
-export default defineConfig({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
+export const datasources = {
+  db: {
+    url: process.env.DATABASE_URL,
   },
-});
+};
